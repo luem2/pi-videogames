8ff857d377a456ebd025b0df9c3aee8a3633090e
Luem <luchemma@gmail.com>
2022-08-01 19:21:38 -0300

    deploy

--- api/src/controllers/genres.js
+++ api/src/controllers/genres.js
@@ -4,17 +4,20 @@ const { API_KEY, API_GENRES_EP } = require('../utility/');
 
 const getGenres = async () => {
   const result = await axios.get(`${API_GENRES_EP}?key=${API_KEY}`);
+  try {
+    result.data.results.forEach(g =>
+      Genre.findOrCreate({
+        where: {
+          name: g.name,
+        },
+      })
+    );
 
-  result.data.results.forEach(g =>
-    Genre.findOrCreate({
-      where: {
-        name: g.name,
-      },
-    })
-  );
-
-  const allGenres = await Genre.findAll();
-  return allGenres;
+    const allGenres = await Genre.findAll();
+    return allGenres;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = getGenres;
